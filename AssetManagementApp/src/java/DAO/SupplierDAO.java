f37d2110eaf5436cedad97dc6f7c18fca47d332a
Kimberly Chong <hsiming.works@gmail.com>
2025-11-19 06:22:27 +0800

    added layout for MVC

---
+++ AssetManagementApp/src/java/DAO/SupplierDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.Supplier;
+import utilities.DBConnection;
+
+/**
+ *
+ * @author kimbe
+ */
+public class SupplierDAO {
+    
+}
